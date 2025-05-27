d0b392754baa9f66ce5d5bb5e789235e9e7054f4
Pedro Jorge Delgadillo Villarroel <pedelevi@gmail.com>
2025-05-27 14:42:50 -0400

    Agrega script de análisis CSV, salida generada, requirements y configuración en .gitignore

--- scripts/hello_csv.py
+++ scripts/hello_csv.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-df = pd.read_csv("data/sample.csv")
-print(df)
-print(df.shape[0])
-print(df["name"])
+df = pd.read_csv("../data/sample.csv")
+df["clasificacion"] = df["age"].apply(lambda x: "mayor" if x >= 18 else "menor")
+df.to_csv("../data/salida.csv", index=False)
+
+print("✅ CSV procesado y guardado como 'salida.csv'")
